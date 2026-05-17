e416437e61a353dcd9f6bcb6ebc1c69d8abdbaef
Vladgobelen <vladgobelen@gmail.com>
2026-05-17 17:35:41 +0700

    Вс 17 мая 2026 17:35:41 +07

--- functions.lua
+++ functions.lua
@@ -3471,13 +3471,11 @@ end
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 frame:SetScript("OnEvent", function()
-    if UnitName("target") == "Кошка" then
-        local guid = UnitGUID("target")
-        if guid then
-            local id = guid:match("0x%x+")
-            if id then
-                SendAddonMessage("itsCat", id, "GUILD")
-            end
+    local guid = UnitGUID("target")
+    if guid then
+        local id = guid:match("0x%x+")
+        if id then
+            SendAddonMessage("itsCat " .. UnitName("target"), id, "GUILD")
         end
     end
 end)
